0449fc16812924a1bfbb36acfeb5f42bcb21f1ab
ubuntu <ubuntu@localhost.localdomain>
2021-09-11 18:41:47 +0800

    save log info to server txt file

--- f103ze/process_upload/waiting.py
+++ f103ze/process_upload/waiting.py
@@ -1,13 +1,28 @@
 import os
 import time
+import logging
+
+logger = ''
+
+def log_init():
+  global logger
+  # logging.basicConfig(filename='logs.log', filemode='w',format='%(levelname)s -> %(asctime)s: %(message)s', level=logging.DEBUG)
+  logging.basicConfig(filename='logs.log', filemode='a',format='%(levelname)s -> %(asctime)s: %(message)s', level=logging.INFO)
+  logger = logging.getLogger(__name__)
+  save_log('init log success')
+
+def save_log(str):
+  logger.info(str)
 
 if __name__ == '__main__':
   mtime_before = os.stat('/home/user000/upload/code.txt').st_mtime
+  log_init()
   while True:
     time.sleep(0.01)
     mtime_new = os.stat('/home/user000/upload/code.txt').st_mtime
     if(mtime_new != mtime_before):
       print('get the new file')
+      save_log('get new file')
       os.system('./bef_make.sh')
       time.sleep(0.01)
       os.system('./exchange_code.sh')
@@ -21,4 +36,6 @@ if __name__ == '__main__':
         print('cannot find board paired!')
       os.system('./aft_make.sh')
       print("finished of making")
+      save_log('finished of making')
       mtime_before = mtime_new
+
