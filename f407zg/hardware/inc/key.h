5f8adae8b8fb53bffd4a4315a2a227a09da385fd
helloworld654 <zhai.shiqiang@163.com>
2021-10-24 22:50:45 +0800

    add ov2640 camera function for f407zg project

--- f407zg/hardware/inc/key.h
+++ f407zg/hardware/inc/key.h
@@ -14,6 +14,8 @@
 //#define KEY2 		GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_2) //PE2
 #define WK_UP 	GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)	//PA0
 
+#define KeyShake_Time  5   //去抖动时间，根据扫描定时器确定长度
+#define KeyShake_overflow  100   //去抖动时间，根据扫描定时器确定长度
 
 /*下面方式是通过位带操作方式读取IO*/
 /*
@@ -30,6 +32,6 @@
 #define WKUP_PRES   4
 
 void KEY_Init(void);	//IO初始化
-u8 KEY_Scan(u8);  		//按键扫描函数	
+void KEY_Scan(void);  		//按键扫描函数	
 
 #endif
--- f407zg/hardware/src/key.c
+++ f407zg/hardware/src/key.c
@@ -67,47 +67,114 @@ void keys_init(char key_n)
 	}
 }
 
-//������������
-//���ذ���ֵ
-//mode:0,��֧��������;1,֧��������;
-//0��û���κΰ�������
-//1��KEY0����
-//2��KEY1����
+//按键处理函数
+//返回按键值
+//mode:0,不支持连续按;1,支持连续按;
+//0，没有任何按键按下
+//1，KEY0按下
+//2，KEY1按下
+//3，KEY2按下 
+//4，KEY3按下 WK_UP
+//注意此函数有响应优先级,KEY0>KEY1>KEY2>KEY3!!
 
-//4��WKUP���� WK_UP
-//ע��˺�������Ӧ���ȼ�,KEY0>KEY1>WK_UP!!
-u8 KEY_Scan(u8 mode)
+uint8_t Key_Flag=0;//按键键值
+
+uint8_t  key_up=1;//按键按松开标志
+
+uint16_t Key_times[3]={0,0,0};//检测到按键按下去的次数
+
+uint16_t Key_Time_out[3]={0,0,0};//如果按键未持续按下，则认为是抖动，抖动超时后，清除按键记次值
+
+
+
+void KEY_Scan(void)
 {	 
-	static u8 key_up=1;//�������ɿ���־
-	if(mode)key_up=1;  //֧������		  
-	if(key_up&&(KEY0==0||KEY1==0||WK_UP==1))
+	
+	if(Key_times[0]>0)
 	{
-		delay_ms(10);//ȥ���� 
-		key_up=0;
-		if(KEY0==0)return 1;
-		else if(KEY1==0)return 2;
-		//else if(KEY2==0)return 3;
-		else if(WK_UP==1)return 4;
-	}else if(KEY0==1&&KEY1==1&&WK_UP==0)key_up=1; 	    
- 	return 0;// �ް�������
+		++Key_Time_out[0];
+		
+		if(Key_Time_out[0]>KeyShake_overflow)//检测是否超时
+    {
+			Key_Time_out[0]=0;//清除超时溢出
+			Key_times[0]=0;   //清除按键记次
+		}
+	}
+	
+	if(Key_times[1]>0)
+	{
+		++Key_Time_out[1];
+		
+		if(Key_Time_out[1]>KeyShake_overflow)//检测是否超时
+    {
+			Key_Time_out[1]=0;//清除超时溢出
+			Key_times[1]=0;   //清除按键记次
+		}
+	}
+	
+	if(Key_times[2]>0)
+	{
+		++Key_Time_out[2];
+		
+		if(Key_Time_out[2]>KeyShake_overflow)//检测是否超时
+    {
+			Key_Time_out[2]=0;//清除超时溢出
+			Key_times[2]=0;   //清除按键记次
+		}
+	}
+	
+		
+	if(KEY0==0||KEY1==0||WK_UP==1)
+	{
+		
+		key_up=0;//当前有按键按下
+		
+    if(WK_UP==1)
+			{
+				 Key_times[0]++;
+				
+				 if(Key_times[0]>=KeyShake_Time)
+				 { 
+					 Key_Time_out[0]=0;//清除超时溢出
+					 Key_times[0]=0;//清除一次计数
+				   Key_Flag=1;//如果连续检测30mS次，检测到按键一直在按下，则确定是按键按下去了
+
+				 }
+			}
+		
+		else if(KEY0==0)
+			{	
+				 Key_times[1]++;
+				
+				 if(Key_times[1]>=KeyShake_Time)
+				 {
+					 Key_Time_out[1]=0;//清除超时溢出
+           Key_times[1]=0;//清除一次计数					 
+				   Key_Flag= 2;      //返回按键值
+			 
+				 } 
+				
+			}
+		else if(KEY1==0)
+			{
+				Key_times[2]++;
+				
+				if(Key_times[2]>=KeyShake_Time)
+				 {
+					 Key_Time_out[2]=0;//清除超时溢出
+           Key_times[2]=0;//清除一次计数					 
+				   Key_Flag= 3;      //返回按键值
+						
+				 }	
+				
+			}
+	}
+	else if(KEY0==1&&KEY1==1&&WK_UP==0)//无任何按键按下
+	{
+		
+		key_up=1;
+		
+	}
+	
+ 		
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
