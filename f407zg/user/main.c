86bc507acf0fa3bcbd0030bfbbe078a95000bf80
helloworld654 <zhai.shiqiang@163.com>
2021-10-27 23:05:42 +0800

    add key function

--- f407zg/user/main.c
+++ f407zg/user/main.c
@@ -9,6 +9,8 @@
 #include "sys.h"
 #include "key.h"
 
+extern uint8_t Key_Flag;//按键键值
+
 int size_C=240,size_R=240;
 u16 rgb_buf[240][240],gray;
 u8 MAX_threshold=47,MIN_threshold=29 ;
@@ -19,6 +21,7 @@ int main(void)
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	uart_init(115200);
 	delay_init(168);
+    TIM3_Int_Init(10000 - 1, 84 - 1); //设置定时器的定时频率为10ms  1秒钟中断100次 for key scan
 	LCD_Init();
 	POINT_COLOR=RED;//设置字体为红色
 	while(OV2640_Init())//初始化OV2640
@@ -31,7 +34,7 @@ int main(void)
 	LCD_ShowString(30,130,200,16,16,"OV2640 OK");
 	delay_ms(100);
 	KEY_Init();
-	OV2640_OutSize_Set(size_C,size_R);
+    OV2640_OutSize_Set(size_C,size_R);
     OV2640_RGB565_Mode();	//RGB565模式
     My_DCMI_Init();			//DCMI配置
     DCMI_DMA_Init((u32)rgb_buf,sizeof(rgb_buf)/4,DMA_MemoryDataSize_HalfWord,DMA_MemoryInc_Enable);//DCMI DMA配置
@@ -63,6 +66,25 @@ int main(void)
                 }
 
             }
+        }
+        if(Key_Flag)	//有按键按下,需要处理
+        {
+            switch(Key_Flag)
+            {
+                case KEY0_PRES:	//对比度设置
+                    printf("key 0\r\n");
+                    break;
+                case KEY1_PRES:	//饱和度Saturation
+                    printf("key 1\r\n");
+                    break;
+                case KEY2_PRES:	//特效设置
+                    printf("key 2\r\n");
+                    break;
+                case WKUP_PRES:	//JPEG输出尺寸设置
+                    printf("key up\r\n");
+                    break;
+            }
+            Key_Flag = 0;
         }
 	}
 }
