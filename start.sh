65e86ccdd2459fcb040b80aa12af8987f269188b
heloworld654 <zhai.shiqiang@163.com>
2022-02-07 21:23:24 +0800

    optimize upload method on server side

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 sudo su - <<EOF
 cd /home/pico/hello8/waiting
-nohup python waiting.py  # keep running when close the ssh connection
+# nohup python waiting.py  # keep running when close the ssh connection,ssh method to upload code
+nohup python3 flask_server_post.py  # keep running when close the ssh connection,flask method to upload code
 EOF
